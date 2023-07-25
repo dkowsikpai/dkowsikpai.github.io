2b94585c34ebcbe41c2ad36eea54614d89c3d880
Kowsik Nandagopan D <dkowsikpai@gmail.com>
2023-07-25 17:02:36 +0530

    update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 if ('serviceWorker' in navigator) {
     // Here we are giving /sw.js because we need a global scope for that file
-    navigator.serviceWorker.register('/sw.js')
-        .then((reg) => {
-            console.log("Service Register Registered", reg);
-        })
-        .catch((err) => {
-            console.error("Service worker failed to register", err);
-        });
+    // navigator.serviceWorker.register('/sw.js')
+    //     .then((reg) => {
+    //         console.log("Service Register Registered", reg);
+    //     })
+    //     .catch((err) => {
+    //         console.error("Service worker failed to register", err);
+    //     });
+
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        for(let registration of registrations) {
+            registration.unregister();
+        } 
+    });
 }
